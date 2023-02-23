c101266ef733e6cfae83456e0a95c9097fb9088e
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-02-23 15:58:39 -0600

    Revision 2
    Figured out mobile nav bar with Michaela as well as my JavaScript.
    Need to adjust spacing on Contact Me button and LinkedIn Logo and add second JavaScript to navbar menu items.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Is running")
 
 $(document).ready(function(){
-  $("contactButton").hover(function(){
+  $(".contactButton").hover(function(){
     $(this).css("background-color", "#013863");
     $(this).css("font-size", "32px");
     }, function(){
